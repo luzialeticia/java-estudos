0eba416073e89ca7298bebd5d65040ede2341378
Letícia Furtado <leticialuzia@icloud.com>
2021-03-31 23:25:39 -0300

    Fazendo a divisão corretamente

--- variaveis-e-fluxo/src/TestaDouble.java
+++ variaveis-e-fluxo/src/TestaDouble.java
@@ -1,2 +1,14 @@
 public class TestaDouble {
+
+    public static void main(String[] args){
+        double salario = 1250.70;
+
+        System.out.println(salario);
+
+        System.out.println("Meu salário é " + salario + ".");
+
+        double divisao = 5.0 / 2;
+
+        System.out.println(divisao);
+    }
 }
