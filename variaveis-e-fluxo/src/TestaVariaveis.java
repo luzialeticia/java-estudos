2b47d9b04b556cd226fe02330d9cfa13624d808d
Letícia Furtado <leticialuzia@icloud.com>
2021-03-31 23:07:26 -0300

    Cria classe para estudar double

--- variaveis-e-fluxo/src/TestaVariaveis.java
+++ variaveis-e-fluxo/src/TestaVariaveis.java
@@ -2,5 +2,10 @@ public class TestaVariaveis {
 
     public static void main(String[] args) {
         System.out.println("Teste");
+
+        int idade;
+        idade = 32;
+
+        System.out.println("A idade é " + idade);
     }
 }
