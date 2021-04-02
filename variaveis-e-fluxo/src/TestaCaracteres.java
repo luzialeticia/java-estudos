e1fca1d8b2c2e3dbd23b5f24c0b7d24a357ac8c3
Letícia Furtado <leticialuzia@icloud.com>
2021-04-02 19:12:22 -0300

    Testando caracteres e valores

---
+++ variaveis-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,18 @@
+public class TestaCaracteres {
+    public static void main(String[] args) {
+        char letra = 'a';
+        System.out.println(letra);
+
+        char valor = 66;
+        System.out.println(valor);
+
+        valor = (char)(valor + 1);
+        System.out.println(valor);
+
+        String frase = "TinkWink, Tipsy, Lala, Po! Teletubies!";
+        System.out.println(frase);
+
+        frase = frase + 2021;
+        System.out.println(frase);
+    }
+}
---
+++ variaveis-e-fluxo/src/TestaValores.java
@@ -0,0 +1,14 @@
+public class TestaValores {
+    public static void main(String[] args) {
+        int primeiro = 5;
+        int segundo = 7;
+        segundo = primeiro;
+
+        System.out.println(primeiro);
+        System.out.println(segundo);
+
+        primeiro = 10;
+
+        System.out.println(primeiro);
+    }
+}
